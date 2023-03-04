8f42c525aa2868754ecea652b449658765a6fe9a
UmarKhan4 <muhammadumarkhan69@gmail.com>
2023-03-04 11:23:49 +0500

    firstcommit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// var dogAge = prompt("what is the age of your dog");
+// var humanAge = (dogAge - 2)*4+21;
+// alert('your dog age ' + humanAge + " in humanage");
+// love cal 
+prompt("enter your name");
+prompt("Enter your cursh name");
+
+let lovePercentage = Math.random() * 100;
+
+lovePercentage = Math.floor(lovePercentage)+1;
+alert(lovePercentage);
+
