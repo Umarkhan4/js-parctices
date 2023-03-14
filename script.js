8e3f519846d94cbdbcdb2be5c11fe9ee1300dfdd
UmarKhan4 <muhammadumarkhan69@gmail.com>
2023-03-14 11:56:28 +0500

    jonas

--- script.js
+++ script.js
@@ -3,11 +3,29 @@
 // var humanAge = (dogAge - 2)*4+21;
 // alert('your dog age ' + humanAge + " in humanage");
 // love cal 
-prompt("enter your name");
-prompt("Enter your cursh name");
+// prompt("enter your name");
+// prompt("Enter your cursh name");
 
-let lovePercentage = Math.random() * 100;
+// let lovePercentage = Math.random() * 100;
 
-lovePercentage = Math.floor(lovePercentage)+1;
-alert(lovePercentage);
-
+// lovePercentage = Math.floor(lovePercentage)+1;
+// alert(lovePercentage);
+// practising the js 
+// const age=15;
+// if(age>=18){
+// console.log('Good you can drive Now👍');
+// } else{
+//    let yearLeft = 18-age;
+// console.log('you have '+ yearLeft + ' years left to drive');
+// }
+let massMark = 93;
+let heightMark = 1.88;
+let massJon = 83;
+let heightjon = 1.79;
+BMIMArk = massMark / heightMark ** 2;
+BMIJon = massJon / (heightjon*heightjon);
+// const MarkHigherBMI= BMIMark > BMIJon;
+if (BMIMArk > BMIJon){
+    console.log('Mark has (${BMIMArk}) the higher BMI');
+} else{
+    console.log('jon has (${BMIJon}) the higher BMI');}
