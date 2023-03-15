645e770ac29f585094a64b5301ddad750e1f4d09
UmarKhan4 <muhammadumarkhan69@gmail.com>
2023-03-15 11:25:31 +0500

    practices

--- script.js
+++ script.js
@@ -8,24 +8,38 @@
 
 // let lovePercentage = Math.random() * 100;
 
-lovePercentage = Math.floor(lovePercentage)+1;
-alert(lovePercentage);
-// practising the js 
-const age=15;
-if(age>=18){
-console.log('Good you can drive Now👍');
+// lovePercentage = Math.floor(lovePercentage)+1;
+// alert(lovePercentage);
+// // practising the js 
+// const age=15;
+// if(age>=18){
+// console.log('Good you can drive Now👍');
+// } else{
+//    let yearLeft = 18-age;
+// console.log('you have '+ yearLeft + ' years left to drive');
+// }
+// let massMark = 93;
+// let heightMark = 1.88;
+// let massJon = 83;
+// let heightjon = 1.79;
+// BMIMArk = massMark / heightMark ** 2;
+// BMIJon = massJon / (heightjon*heightjon);
+// // const MarkHigherBMI= BMIMark > BMIJon;
+// if (BMIMArk > BMIJon){
+//     console.log('Mark has (${BMIMArk}) the higher BMI');
+// } else{
+//     console.log('jon has (${BMIJon}) the higher BMI');}
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('umar'));
+console.log(Boolean({}));
+let money ;
+if (money){
+    console.log('Great you have the money');
 } else{
-   let yearLeft = 18-age;
-console.log('you have '+ yearLeft + ' years left to drive');
+    console.log('you are short of money');
 }
-let massMark = 93;
-let heightMark = 1.88;
-let massJon = 83;
-let heightjon = 1.79;
-BMIMArk = massMark / heightMark ** 2;
-BMIJon = massJon / (heightjon*heightjon);
-// const MarkHigherBMI= BMIMark > BMIJon;
-if (BMIMArk > BMIJon){
-    console.log('Mark has (${BMIMArk}) the higher BMI');
-} else{
-    console.log('jon has (${BMIJon}) the higher BMI');}
+
+let age = '18';
+if (age === 18) console.log('you becae adult just now ');
+if (age == 18) console.log('just adult');
