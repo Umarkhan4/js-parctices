e95764b542a1519944056f9b438a8a82c544c18a
UmarKhan4 <muhammadumarkhan69@gmail.com>
2023-03-20 12:38:45 +0500

    practice

--- script.js
+++ script.js
@@ -29,17 +29,70 @@
 //     console.log('Mark has (${BMIMArk}) the higher BMI');
 // } else{
 //     console.log('jon has (${BMIJon}) the higher BMI');}
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('umar'));
-console.log(Boolean({}));
-let money ;
-if (money){
-    console.log('Great you have the money');
-} else{
-    console.log('you are short of money');
-}
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('umar'));
+// console.log(Boolean({}));
+// let money ;
+// if (money){
+//     console.log('Great you have the money');
+// } else{
+//     console.log('you are short of money');
+// }
 
-let age = '18';
-if (age === 18) console.log('you becae adult just now ');
-if (age == 18) console.log('just adult');
+// let age = '18';
+// if (age === 18) console.log('you becae adult just now ');
+// if (age == 18) console.log('just adult');
+//  const hasDrivinglicen = true;
+//  const goodVision = true;
+//  console.log(hasDrivinglicen && goodVision );
+//  console.log(hasDrivinglicen || goodVision );
+//  console.log(!hasDrivinglicen);
+//   let scoreDolphines = (800+90+15) /3;
+//   let scoreKoales = Math.floor((800+90+15) /3);
+//   console.log(scoreDolphines, scoreKoales);
+//   if (scoreDolphines > scoreKoales && scoreDolphines>=100){
+//       console.log('Dolphines has won the match!');
+//   } else if (scoreKoales > scoreDolphines && scoreKoales>=100){
+//     console.log('Kolasea has won👏');
+
+//   } else {
+//     console.log('Both the teams has won');
+
+//   }
+let day ='monday';
+switch (day)  {
+    case  'monday':
+        console.log('the first dY OF THE WEEK');
+        break;
+        case  'Tuesday':
+        console.log('the mazay dY OF THE WEEK');
+        break;
+        case  'wednesday':
+        console.log('the gusht dY OF THE WEEK');
+        break;
+        case  'Thursday':
+        console.log('the outting dY OF THE WEEK');
+        break;
+        case  'Friday':
+            case  'Saturday':
+                case  'sunday':
+        console.log('the Last days OF THE WEEK');
+        break;
+        default:
+            console.log('invalid input');
+}
+let weekDays ='Friday';
+  if(weekDays=== 'monday'){
+    console.log('the first dY OF THE WEEK');
+  }else if(weekDays === 'tuesday'){
+    console.log('the mazay dY OF THE WEEK');
+  }else if(weekDays === 'Wednesday'){
+    console.log('the gusht dY OF THE WEEK');
+  }else if(weekDays === 'thursday'){
+    console.log('the outting dY OF THE WEEK');
+  }else if(weekDays === 'Friday' || weekDays ==='saturday'||weekDays === 'sunday'){
+    console.log('the Last days OF THE WEEK');
+  }else{
+    console.log('invalid');
+  }
