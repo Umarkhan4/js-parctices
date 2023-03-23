ca1bbb51f3ef656bdd0f895dbd6868eafdebd929
UmarKhan4 <muhammadumarkhan69@gmail.com>
2023-03-23 11:10:32 +0500

    correct

--- script.js
+++ script.js
@@ -107,4 +107,4 @@
 
  const bill = 275;
  const tip = bill <=300 && bill >=50 ? bill*0.15 : bill *0.2; 
- console.log('your bill was ${bill}, your tip was ${tip}, an total value is ${bill + tip}');
+ console.log(`your bill was ${bill}, your tip was ${tip}, an total value is ${bill + tip}`);
